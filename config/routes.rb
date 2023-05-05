e573edc07c585a89807f83393d7a2c951fda81e1
manh <manhdd@its-global.vn>
2023-05-05 17:33:29 +0700

    first

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # root 'static_pages#home'
+  # devise_for :users
+  devise_for :users
+  root 'main#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
